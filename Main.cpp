3ed464652a7add263903500bb79c944419a2abb5
Fredrik Orderud <forderud@gmail.com>
2019-01-07 15:18:31 +0100

    restructure

--- Main.cpp
+++ Main.cpp
@@ -1,55 +1,54 @@
 #include <iostream>
-#include <stdexcept>
-#include <comdef.h> // for _com_error
 #include <atlbase.h>
 
 
-/** Translate COM HRESULT failure into exceptions. */
-static void CHECK(HRESULT hr) {
-    if (FAILED(hr)) {
-        _com_error err(hr);
-        const wchar_t * msg = err.ErrorMessage(); // weak ptr.
+void CreateAsUser (wchar_t* progid, wchar_t* user, wchar_t* passwd) {
+    // initialize multi-threaded COM apartment
+    if (FAILED(CoInitializeEx(NULL, COINIT_MULTITHREADED)))
         abort();
+
+    // impersonate a different user
+    HANDLE user_token = {};
+    if (!LogonUser(user, L""/*domain*/, passwd, LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, &user_token)) {
+        auto err = GetLastError(); abort();
     }
+    if (!ImpersonateLoggedOnUser(user_token)) {
+        auto err = GetLastError(); abort();
+    }
+
+    // attempt to disable COM security and enable cloaking
+    HRESULT hr = CoInitializeSecurity(nullptr, -1/*auto*/, nullptr, NULL, RPC_C_AUTHN_LEVEL_NONE, RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_STATIC_CLOAKING, NULL);
+    if (FAILED(hr))
+        abort();
+
+    // create COM object in a separate process (fails with 0x80080005: Server execution failed)
+    CComPtr<IUnknown> obj;
+#ifdef DEBUG_COM_ACTIVATION
+    // open Event Viewer, "Windows Logs" -> "System" log to see details on failures
+    CLSID clsid = {};
+    CLSIDFromProgID(progid, &clsid);
+    CComPtr<IClassFactory> cf;
+    hr = CoGetClassObject(clsid, CLSCTX_LOCAL_SERVER | CLSCTX_ENABLE_CLOAKING, NULL, IID_IClassFactory, (void**)&cf);
+    if (FAILED(hr))
+        abort();
+    hr = cf->CreateInstance(nullptr, IID_IUnknown, (void**)&obj);
+    if (FAILED(hr))
+        abort();
+#else
+    hr = obj.CoCreateInstance(progid, nullptr, CLSCTX_LOCAL_SERVER | CLSCTX_ENABLE_CLOAKING);
+    if (FAILED(hr))
+        abort();
+#endif
 }
 
 
 int wmain(int argc, wchar_t *argv[]) {
-    if (argc < 3) {
+    if (argc < 4) {
         std::cerr << "Too few arguments\n.";
         std::cerr << "Usage: ComImpersonation.exe <ProgID> <username> <password>" << std::endl;
         return -1;
     }
 
-    // initialize multi-threaded COM apartment
-    CHECK(CoInitializeEx(NULL, COINIT_MULTITHREADED));
-
-    // impersonate a different user
-    CHandle user_token;
-    {
-        if (!LogonUser(argv[2]/*user*/, L""/*domain*/, argv[3]/*passwd*/, LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, &user_token.m_h)) {
-            auto err = GetLastError(); abort();
-        }
-        if (!ImpersonateLoggedOnUser(user_token)) {
-            auto err = GetLastError(); abort();
-        }
-    }
-
-    // attempt to disable COM security
-    CHECK(CoInitializeSecurity(nullptr, -1/*auto*/, nullptr, NULL, RPC_C_AUTHN_LEVEL_NONE, RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_STATIC_CLOAKING, NULL));
-
-    // create COM object in a separate process
-    CComPtr<IUnknown> obj;
-    DWORD class_context = CLSCTX_LOCAL_SERVER | CLSCTX_ENABLE_CLOAKING; // | CLSCTX_ENABLE_AAA;
-#ifdef DEBUG_COM_ACTIVATION
-    // open Event Viewer, "Windows Logs" -> "System" log to see details on failures
-    CLSID clsid = {};
-    CHECK(CLSIDFromProgID(argv[1], &clsid));
-    CComPtr<IClassFactory> cf;
-    CHECK(CoGetClassObject(clsid, class_context, NULL, IID_IClassFactory, (void**)&cf));
-    CHECK(cf->CreateInstance(nullptr, IID_IUnknown, (void**)&obj));
-#else
-    CHECK(obj.CoCreateInstance(argv[1], nullptr, class_context));
-#endif
+    CreateAsUser(argv[1], argv[2], argv[3]);
     std::cout << "Object created" << std::endl;
 }
