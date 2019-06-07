87ded98e1ae476aa0e90e5620d3e2dc2127da58d
Fredrik Orderud <fredrik.orderud@ge.com>
2019-06-07 18:00:33 +0200

    Annotate TestControl with CATID_AppContainerCompatible
    This triggers generation of a HKCR\CLSID\{F0DFBE77-1697-428E-A895-EFEE202B9333}\Implemented Categories\{59FB2056-D625-48D0-A944-1A85B5AB2640} registry folder.

--- TestControl/TestControl.hpp
+++ TestControl/TestControl.hpp
@@ -3,6 +3,9 @@
 #include "Resource.h"
 #include "TestControl_h.h"
 
+// copied from https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/compatibility/dn519894(v=vs.85)
+DEFINE_GUID(CATID_AppContainerCompatible, 0x59fb2056, 0xd625, 0x48d0, 0xa9, 0x44, 0x1a, 0x85, 0xb5, 0xab, 0x26, 0x40);
+
 
 class ATL_NO_VTABLE TestControl :
     public CComObjectRootEx<CComMultiThreadModel>, // also compatible with single-threaded apartment
@@ -27,6 +30,9 @@ public:
         COM_INTERFACE_ENTRY(ISimpleCalculator)
     END_COM_MAP()
 
+    BEGIN_CATEGORY_MAP(TestControl)
+        IMPLEMENTED_CATEGORY(CATID_AppContainerCompatible)
+    END_CATEGORY_MAP()
 private:
 };
 
