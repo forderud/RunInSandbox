87f65815f452e339e44eacf1fb14c1f0c9749f2f
Fredrik Orderud <forderud@gmail.com>
2019-08-10 16:24:29 +0200

    Fix sporadic failures by postponing "SD" delete.

--- MakeLowIntegrity/MakeLowIntegrity.cpp
+++ MakeLowIntegrity/MakeLowIntegrity.cpp
@@ -24,42 +24,28 @@ static void WIN32_CHECK(BOOL res) {
     throw std::runtime_error(msg);
 }
 
-/// sporadic error codes experienced with SetNamedSecurityInfo
-const static DWORD SPORADIC_FAILURE_CODES[]{
-    ERROR_INSUFFICIENT_BUFFER, ///< 122
-    ERROR_INVALID_SID,         ///< 1337
-    ERROR_BAD_INHERITANCE_ACL, ///< 1340
-};
-
 
 /** Tag a folder path as writable by low-integrity processes.
     By default, only %USER PROFILE%\AppData\LocalLow is writable.
     Based on "Designing Applications to Run at a Low Integrity Level" https://msdn.microsoft.com/en-us/library/bb625960.aspx */
 static DWORD MakePathLowIntegrity(const WCHAR * path) {
-    DWORD ret = 0;
-    do {
-        ACL * sacl = nullptr; // system access control list
-        {
-            // initialize "low integrity" System Access Control List (SACL)
-            PSECURITY_DESCRIPTOR SD = nullptr;
-            // Security Descriptor String interpretation: (based on sddl.h)
-            // SACL:(ace_type=Integrity label; ace_flags=; rights=SDDL_NO_WRITE_UP; object_guid=; inherit_object_guid=; account_sid=Low mandatory level)
-            WIN32_CHECK(ConvertStringSecurityDescriptorToSecurityDescriptorW(L"S:(ML;;NW;;;LW)", SDDL_REVISION_1, &SD, NULL));
-            BOOL sacl_present = FALSE;
-            BOOL sacl_defaulted = FALSE;
-            WIN32_CHECK(GetSecurityDescriptorSacl(SD, &sacl_present, &sacl, &sacl_defaulted));
-            LocalFree(SD);
-        }
+    ACL * sacl = nullptr; // system access control list
+    PSECURITY_DESCRIPTOR SD = nullptr;
+    {
+        // initialize "low integrity" System Access Control List (SACL)
+        // Security Descriptor String interpretation: (based on sddl.h)
+        // SACL:(ace_type=Integrity label; ace_flags=; rights=SDDL_NO_WRITE_UP; object_guid=; inherit_object_guid=; account_sid=Low mandatory level)
+        WIN32_CHECK(ConvertStringSecurityDescriptorToSecurityDescriptorW(L"S:(ML;;NW;;;LW)", SDDL_REVISION_1, &SD, NULL));
+        BOOL sacl_present = FALSE;
+        BOOL sacl_defaulted = FALSE;
+        WIN32_CHECK(GetSecurityDescriptorSacl(SD, &sacl_present, &sacl, &sacl_defaulted));
+    }
 
-        // apply "low integrity" SACL
-        ret = SetNamedSecurityInfoW(const_cast<WCHAR*>(path), SE_FILE_OBJECT, LABEL_SECURITY_INFORMATION, /*owner*/NULL, /*group*/NULL, /*Dacl*/NULL, sacl);
-        if (ret == ERROR_SUCCESS)
-            return ret;
-
-        bool sporadic_failure = std::find(std::begin(SPORADIC_FAILURE_CODES), std::end(SPORADIC_FAILURE_CODES), ret) != std::end(SPORADIC_FAILURE_CODES);
-        if (!sporadic_failure)
-            return ret;
-    } while (true);
+    // apply "low integrity" SACL
+    DWORD ret = SetNamedSecurityInfoW(const_cast<WCHAR*>(path), SE_FILE_OBJECT, LABEL_SECURITY_INFORMATION, /*owner*/NULL, /*group*/NULL, /*Dacl*/NULL, sacl);
+    LocalFree(SD);
+    if (ret == ERROR_SUCCESS)
+        return ret; // success
 
     // ERROR_FILE_NOT_FOUND ///< 2
     // ERROR_ACCESS_DENIED  ///< 5
