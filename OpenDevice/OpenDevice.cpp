ba865b389dcc384caf62cb299cd30084164e8da7
Fredrik Orderud <fredrik.orderud@ge.com>
2019-10-10 09:59:36 +0200

    Add HANDLE RAII wrapper.

--- OpenDevice/OpenDevice.cpp
+++ OpenDevice/OpenDevice.cpp
@@ -2,6 +2,30 @@
 #include <string>
 #include <Windows.h>
 
+/** RAII wrapper of Win32 "handle" objects. */
+class HandleWrap {
+public:
+    HandleWrap(HANDLE h) : handle(h) {
+    }
+
+    ~HandleWrap() {
+        if (handle) {
+            CloseHandle(handle);
+            handle = nullptr;
+        }
+    }
+
+    operator HANDLE () {
+        return handle;
+    }
+
+private:
+    HandleWrap(const HandleWrap &) = delete;
+    HandleWrap& operator = (const HandleWrap &) = delete;
+
+    HANDLE handle = nullptr;
+};
+
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -11,7 +35,7 @@ int main(int argc, char *argv[]) {
 
     std::string port = argv[1]; // e.g. "COM3";
 
-    HANDLE handle = CreateFileA(port.c_str(), GENERIC_READ | GENERIC_WRITE, /*no sharing*/0,
+    HandleWrap handle = CreateFileA(port.c_str(), GENERIC_READ | GENERIC_WRITE, /*no sharing*/0,
                                 /*no security*/NULL, OPEN_EXISTING, /*no overlapped*/0, NULL);
     if (!handle) {
         std::cerr << "Unable to open " << port << "\n";
@@ -19,9 +43,10 @@ int main(int argc, char *argv[]) {
     }
     std::cout << "Sucessfully opened " << port << "\n";
 
+    // attempt to write to device
     char buffer[] = "X";
     DWORD bytesWritten = 0;
-    BOOL ok = WriteFile(handle, buffer, sizeof(buffer), &bytesWritten, NULL);
+    BOOL ok = WriteFile(handle, buffer, sizeof(buffer), &bytesWritten, /*no overlapped*/NULL);
     if (!ok) {
         std::cerr << "Write failed\n";
         return -1;
