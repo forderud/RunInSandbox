034f5d93d996e43b152bbdf8c8fb5c423bec3d10
Fredrik Orderud <fredrik.orderud@ge.com>
2020-10-05 11:20:00 +0200

    Make TestControl compatible with elevated activation.

--- TestControl/Resource.h
+++ TestControl/Resource.h
@@ -1,5 +1,5 @@
 #define IDS_PROJNAME                    100
 #define IDR_TESTCONTROL                 101
-
+#define IDR_APP_NAME_MUI_STR            102
 #define IDR_AppID                       106
 #define IDR_TestControl                 107
