318af0c4d7217ac1f3c0c7b249cc1a6c2daa1232
Fredrik Orderud <fredrik.orderud@ge.com>
2020-10-30 16:35:32 +0100

    fix semi-broken elevation check.

--- TestControl/TestControl.cpp
+++ TestControl/TestControl.cpp
@@ -3,6 +3,38 @@
 #include "../RunInSandbox/ComCreate.hpp"
 
 
+static bool IsProcessElevated () {
+#if 1
+    // Determine the integrity level for a process.
+    // Based on https://github.com/chromium/chromium/blob/master/base/process/process_info_win.cc */
+    HANDLE process_token = GetCurrentProcessToken();
+    DWORD token_info_length = 0;
+    if (GetTokenInformation(process_token, TokenIntegrityLevel, NULL, 0, &token_info_length))
+        abort();
+
+    std::vector<char> token_info_buf(token_info_length);
+    auto* token_info = reinterpret_cast<TOKEN_MANDATORY_LABEL*>(token_info_buf.data());
+    if (!GetTokenInformation(process_token, TokenIntegrityLevel, token_info, token_info_length, &token_info_length))
+        abort();
+
+    DWORD integrity_level = *GetSidSubAuthority(token_info->Label.Sid, *GetSidSubAuthorityCount(token_info->Label.Sid)-1);
+    return (integrity_level >= SECURITY_MANDATORY_HIGH_RID);
+#else
+    // TODO: Seem to always return true if the parent process is elevated
+    HandleWrap token;
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &token))
+        return E_ACCESSDENIED;
+
+    TOKEN_ELEVATION elevation = {};
+    DWORD cbSize = sizeof(TOKEN_ELEVATION);
+    if (!GetTokenInformation(token, TokenElevation, &elevation, sizeof(elevation), &cbSize))
+        return E_ACCESSDENIED;
+
+    return elevation.TokenIsElevated;
+#endif
+}
+
+
 TestControl::TestControl(){
 }
 
@@ -15,16 +47,7 @@ HRESULT STDMETHODCALLTYPE TestControl::Add(int a, int b, int * sum) {
 }
 
 HRESULT STDMETHODCALLTYPE TestControl::IsElevated (/*out*/BOOL * is_elevated) {
-    HandleWrap token;
-    if (!OpenProcessToken(GetCurrentProcess( ), TOKEN_QUERY, &token))
-        return E_ACCESSDENIED;
-
-    TOKEN_ELEVATION elevation = {};
-    DWORD cbSize = sizeof(TOKEN_ELEVATION);
-    if (!GetTokenInformation(token, TokenElevation, &elevation, sizeof(elevation), &cbSize))
-        return E_ACCESSDENIED;
-
-    *is_elevated = elevation.TokenIsElevated;
+    *is_elevated = IsProcessElevated();
     return S_OK;
 }
 
