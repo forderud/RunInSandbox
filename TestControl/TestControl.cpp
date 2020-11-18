5956dca7dbf0205450f8cb86cf45c46fee7c94ff
Fredrik Orderud <fredrik.orderud@ge.com>
2020-11-18 14:43:08 +0100

    Move socket com. to a separate header.

--- TestControl/TestControl.cpp
+++ TestControl/TestControl.cpp
@@ -1,8 +1,6 @@
 #include "TestControl.hpp"
-#include <Shlobj.h>
 #include "../RunInSandbox/ComCreate.hpp"
-
-#pragma comment (lib, "Ws2_32.lib")
+#include "Socket.hpp"
 
 
 TestControl::TestControl(){
@@ -24,56 +22,6 @@ HRESULT STDMETHODCALLTYPE TestControl::IsElevated (/*out*/BOOL * is_elevated, /*
 
     return S_OK;
 }
-
-
-class SocketWrap {
-public:
-    SocketWrap() {
-        WSADATA init_data = {};
-        WSAStartup(MAKEWORD(2, 2), &init_data);
-
-        m_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if(m_sock == INVALID_SOCKET) {
-            //printf("Failed to create socket, error code: %d\n", WSAGetLastError());
-            throw std::runtime_error("Failed to create socket");
-        }
-    }
-
-    ~SocketWrap() {
-        if (m_sock == INVALID_SOCKET)
-            return;
-
-        int res = closesocket(m_sock);
-        if (res)
-            std::terminate();
-        m_sock = INVALID_SOCKET;
-
-        res = WSACleanup();
-        if (res == SOCKET_ERROR)
-            std::terminate();
-    }
-
-    bool TryToConnect (const std::string& host, const uint16_t port) {
-        sockaddr_in addr = {};
-        addr.sin_family = AF_INET;
-        addr.sin_addr.s_addr = inet_addr(host.c_str());
-        addr.sin_port = htons(port);
-
-        if(connect(m_sock, (SOCKADDR *)&addr, sizeof(addr)) == SOCKET_ERROR) {
-            if(WSAGetLastError() == WSAEACCES)
-                ; // Connection blocked
-            else
-                ; // Connection unsuccessful but not blocked
-
-            return false;
-        }
-
-        return true;
-    }
-
-private:
-    SOCKET m_sock = INVALID_SOCKET;
-};
 
 
 HRESULT STDMETHODCALLTYPE TestControl::TestNetworkConnection (/*in*/BSTR host, USHORT port, /*out*/BOOL * can_access) {
