2ce9015bfd7b67166554916df5e4de3b548e03c7
Fredrik Orderud <fredrik.orderud@ge.com>
2020-11-19 13:49:01 +0100

    Add support function for granting all AppContainers access to a path.

--- GrantAccess/Main.cpp
+++ GrantAccess/Main.cpp
@@ -16,15 +16,26 @@ int wmain(int argc, wchar_t *argv[]) {
     if (mode == L"li") {
         std::wcout << L"Making path low-integrity: " << path << std::endl;
         DWORD err = MakePathLowIntegrity(path.c_str());
-        if (err) {
+        if (err != ERROR_SUCCESS) {
             _com_error error(err);
             std::wcerr << L"ERROR: " << error.ErrorMessage() << L" (" << err << L")" << std::endl;
             return -2;
         }
     } else if (mode == L"ac") {
+        std::wcout << L"Making path accible by AppContainers: " << path << std::endl;
+        //sid = LocalAlloc(LPTR, SECURITY_MAX_SID_SIZE);
+        //DWORD sidListSize = SECURITY_MAX_SID_SIZE;
+        //WIN32_CHECK(CreateWellKnownSid(WinBuiltinAnyPackageSid, NULL, &sid, &sidListSize));
 
+        SidWrap sid;
+        WIN32_CHECK(ConvertStringSidToSid(L"S-1-15-2-1", &sid)); // ALL_APP_PACKAGES
 
-
+        DWORD err = MakePathAppContainer(sid, path.c_str());
+        if (err != ERROR_SUCCESS) {
+            _com_error error(err);
+            std::wcerr << L"ERROR: " << error.ErrorMessage() << L" (" << err << L")" << std::endl;
+            return -2;
+        }
     } else {
         std::wcerr << L"ERROR: Unknown mode " << mode << std::endl;
         return -2;
