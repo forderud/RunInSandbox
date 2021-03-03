97e3878160651f7964524550ff97eb9aaa478515
Fredrik Orderud <fredrik.orderud@ge.com>
2021-03-03 13:26:13 +0100

    Switch to single-threaded aparment (STA). Done to better mimic the behavior of a GUI app.

--- RunInSandbox/Main.cpp
+++ RunInSandbox/Main.cpp
@@ -70,7 +70,7 @@ int wmain (int argc, wchar_t *argv[]) {
 
     if (progid_provided) {
         // initialize multi-threaded COM apartment
-        ComInitialize com(COINIT_MULTITHREADED);
+        ComInitialize com(COINIT_APARTMENTTHREADED);
 
         std::wcout << L"Creating COM object " << progid << L" in " << ToString(mode).c_str() << L"...\n";
 
