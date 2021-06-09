58931e4e29dd0f39d041bea4a7cce0f1e021d58c
Fredrik Orderud <fredrik.orderud@ge.com>
2021-06-09 18:38:13 +0200

    Delete disabled WindowStation & Desktop permission code, since it didn't appear to be relevant for the SetCursorPos UIPI problem.

--- TestControl/TestControl.cpp
+++ TestControl/TestControl.cpp
@@ -80,68 +80,8 @@ HRESULT STDMETHODCALLTYPE TestControl::TestCallback(IUnknown * obj) {
     return tmp->Ping();
 }
 
-/** Window Station switching RAII wrapper. */
-class WindowStation {
-public:
-    WindowStation(const wchar_t* name) {
-        prev_winsta = GetProcessWindowStation();
-        assert(prev_winsta);
-
-        ACCESS_MASK access = WINSTA_ACCESSCLIPBOARD |WINSTA_ACCESSGLOBALATOMS |WINSTA_CREATEDESKTOP |WINSTA_ENUMDESKTOPS |WINSTA_ENUMERATE |WINSTA_EXITWINDOWS |WINSTA_READATTRIBUTES |WINSTA_READSCREEN |WINSTA_WRITEATTRIBUTES;
-        cur_winsta = OpenWindowStationW(name, false, access);
-        assert(cur_winsta);
-
-        BOOL ok = SetProcessWindowStation(cur_winsta);
-        assert(ok);
-    }
-
-    ~WindowStation() {
-        // switch back to prev. window station
-        SetProcessWindowStation(prev_winsta);
-
-        if (cur_winsta)
-            CloseWindowStation(cur_winsta);
-    }
-private:
-    HWINSTA prev_winsta = nullptr; ///< weak ptr. don't need to close
-    HWINSTA cur_winsta = nullptr;
-};
-
-/** Desktop switching RAII wrapper. */
-class Desktop {
-public:
-    Desktop(const wchar_t* name) {
-        prev_desk = GetThreadDesktop(GetCurrentThreadId());
-        assert(prev_desk);
-
-        ACCESS_MASK access = DESKTOP_CREATEMENU |DESKTOP_CREATEWINDOW |DESKTOP_ENUMERATE |DESKTOP_HOOKCONTROL |DESKTOP_JOURNALPLAYBACK |DESKTOP_JOURNALRECORD |DESKTOP_READOBJECTS |DESKTOP_SWITCHDESKTOP |DESKTOP_WRITEOBJECTS;
-        cur_desk = OpenDesktopW(name, NULL, false, access);
-        assert(cur_desk);
-
-        BOOL ok = SetThreadDesktop(cur_desk);
-        assert(ok);
-    }
-    ~Desktop() {
-        // switch back to prev. desktop
-        SetThreadDesktop(prev_desk);
-
-        if (cur_desk)
-            CloseDesktop(cur_desk);
-    }
-private:
-    HDESK prev_desk = nullptr; ///< weak ptr. don't need to close
-    HDESK cur_desk = nullptr;
-};
 
 HRESULT STDMETHODCALLTYPE TestControl::MoveMouseCursor(int x_pos, int y_pos) {
-#if 0
-    // This does NOT fix failing SetCursorPos in medium IL if the parent process is high IL
-    // switch to window station "Winsta0"
-    WindowStation winsta(L"Winsta0");
-    // switch to desktop "default"
-    Desktop desk(L"default");
-#endif
-
     // will fail without WINSTA_WRITEATTRIBUTES access
     // will fail if the foreground window is running at higher IL than this process (UIPI limitation)
     BOOL ok = SetCursorPos(x_pos, y_pos);
