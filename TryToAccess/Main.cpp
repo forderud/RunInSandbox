551a960bc76e07ccfab730da954c699cc06d759b
Fredrik Orderud <fredrik.orderud@ge.com>
2021-09-05 14:43:12 +0200

    Fix broken build after HandleWrap replacement.

--- TryToAccess/Main.cpp
+++ TryToAccess/Main.cpp
@@ -8,22 +8,23 @@
 
 
 static void TryOpenFile (std::wstring path) {
-    HandleWrap handle;
-    handle = CreateFile2(path.c_str(), GENERIC_READ | GENERIC_WRITE, /*no sharing*/0, OPEN_EXISTING, NULL);
-    if (!handle)
+    using Handle = Microsoft::WRL::Wrappers::HandleT<Microsoft::WRL::Wrappers::HandleTraits::HANDLETraits>; // INVALID_HANDLE_VALUE on failure
+
+    Handle handle(CreateFile2(path.c_str(), GENERIC_READ | GENERIC_WRITE, /*no sharing*/0, OPEN_EXISTING, NULL));
+    if (!handle.IsValid())
         throw std::runtime_error("Unable to open file");
 
     // attempt to read from device
     char buffer[1] = {};
     DWORD bytesRead = 0;
-    BOOL ok = ReadFile(handle, buffer, sizeof(buffer), &bytesRead, /*no overlapped*/NULL);
+    BOOL ok = ReadFile(handle.Get(), buffer, sizeof(buffer), &bytesRead, /*no overlapped*/NULL);
     if (!ok || (bytesRead == 0))
         throw std::runtime_error("Read failed");
 
     // attempt to write to device
     buffer[0] = 'X';
     DWORD bytesWritten = 0;
-    ok = WriteFile(handle, buffer, sizeof(buffer), &bytesWritten, /*no overlapped*/NULL);
+    ok = WriteFile(handle.Get(), buffer, sizeof(buffer), &bytesWritten, /*no overlapped*/NULL);
     if (!ok || (bytesWritten == 0))
         throw std::runtime_error("Write failed");
 }
