8b145f52b011d19e9413d948160bf4dd446cb370
Fredrik Orderud <fredrik.orderud@ge.com>
2023-12-27 20:44:30 +0100

    Start on COM sandboxing from .Net.

---
+++ RunInSandboxNet/Program.cs
@@ -0,0 +1,11 @@
+﻿string progId;
+if (args.Length > 0)
+    progId = args[0];
+else
+    progId = "TestControl.TestControl"; // default COM server
+
+var obj = appapi.Sandboxing.CreateLowIL(progId);
+obj = null;
+
+GC.Collect();
+GC.WaitForPendingFinalizers();
