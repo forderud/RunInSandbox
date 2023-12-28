d87eae3cf5935e0605e47929d1deb34a766945e3
Fredrik Orderud <fredrik.orderud@ge.com>
2023-12-28 16:04:54 +0100

    Restructure impl.

--- RunInSandboxNet/Program.cs
+++ RunInSandboxNet/Program.cs
@@ -1,19 +1,30 @@
-﻿string progId;
-if (args.Length > 0)
-    progId = args[0];
-else
-    progId = "TestControl.TestControl"; // default COM server
-
-var obj = Sandboxing.CoCreate(Sandboxing.SDDL_ML_LOW, progId);
-
+﻿class RunInSandbox
 {
-    // Exercise TestControl API
-    var tc = (TestControl.ITestInterface)obj;
-    bool isElevated, isHighIL;
-    tc.IsElevated(out isElevated, out isHighIL);
+    static void TestCreate(string level, string progId)
+    {
+        var obj = Sandboxing.CoCreate(level, progId);
+
+        {
+            // Exercise TestControl API
+            var tc = (TestControl.ITestInterface)obj;
+            bool isElevated, isHighIL;
+            tc.IsElevated(out isElevated, out isHighIL);
+            Console.WriteLine("High IL: " + isHighIL);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string progId;
+        if (args.Length > 0)
+            progId = args[0];
+        else
+            progId = "TestControl.TestControl"; // default COM server
+
+
+        TestCreate(Sandboxing.SDDL_ML_LOW, progId);
+
+        GC.Collect();
+        //GC.WaitForPendingFinalizers();
+    }
 }
-
-obj = null;
-
-GC.Collect();
-GC.WaitForPendingFinalizers();
