a075ed8bf0325cb8cd8e526bb9f366a3df26b7f7
Fredrik Orderud <fredrik.orderud@ge.com>
2023-12-29 17:24:34 +0100

    WIP: Start using DuplicateTokenEx
    Doesn't work yet.

--- RunInSandboxNet/Sandboxing.cs
+++ RunInSandboxNet/Sandboxing.cs
@@ -12,16 +12,22 @@ class Sandboxing
     public static readonly string SDDL_ML_LOW = "LW"; // Low mandatory level
     public static readonly string SDDL_ML_MEDIUM = "ME"; // Medium integrity level
 
-    /** Create COM server in a sandboxed process.
-     *  WARNING: WindowsIdentity.GetCurrent throws "Access is denied" exception if called multiple times.
-     *  Associated bug report: https://github.com/dotnet/runtime/issues/96397 */
+    /** Create COM server in a sandboxed process. */
     public static object CoCreate(string level, Type clsid)
     {
         // mimic OpenProcessToken(GetCurrentProcess(), TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_ADJUST_DEFAULT)
-        using WindowsIdentity curId = WindowsIdentity.GetCurrent(TokenAccessLevels.Duplicate | TokenAccessLevels.Impersonate | TokenAccessLevels.Query | TokenAccessLevels.AdjustDefault);
+        SafeAccessTokenHandle curToken;
+#if false
+        using WindowsIdentity id = WindowsIdentity.GetCurrent(TokenAccessLevels.Duplicate | TokenAccessLevels.Impersonate | TokenAccessLevels.Query | TokenAccessLevels.AdjustDefault);
+        curToken = id.AccessToken;
+#else
+        if (!OpenProcessToken(GetCurrentProcess(), TokenAccessLevels.Duplicate | TokenAccessLevels.Impersonate | TokenAccessLevels.Query | TokenAccessLevels.AdjustDefault, out curToken))
+            throw new Win32Exception("OpenProcessToken failed");
+#endif
         // mimic DuplicateTokenEx(curToken, 0, NULL, SecurityImpersonation, TokenImpersonation, &token)
-        using var id = (WindowsIdentity)curId.Clone();
-        using SafeAccessTokenHandle token = id.AccessToken; // copy of process token
+        var token = new SafeAccessTokenHandle();
+        if (!DuplicateTokenEx(curToken, 0, IntPtr.Zero, SECURITY_IMPERSONATION_LEVEL.SecurityImpersonation, TokenType.TokenImpersonation, ref token))
+            throw new Win32Exception("DuplicateTokenEx failed");
 
         {
             IntPtr sidPtr = IntPtr.Zero;
@@ -37,9 +43,16 @@ class Sandboxing
             Marshal.FreeHGlobal(sidPtr); // LocalFree wrapper
         }
 
+        SafeAccessTokenHandle token2;
+#if false
+        using var id = new WindowsIdentity(token.DangerousGetHandle());
+        token2 = id.AccessToken;
+#else
+        token2 = token;
+#endif
         // RunImpersonated isn't actually needed here, since Process.Start & Activator.CreateInstance
         // are using the current process token, and _not_ the impersonation token.
-        return WindowsIdentity.RunImpersonated(token, () =>
+        object obj = WindowsIdentity.RunImpersonated(token2, () =>
         {
             // process start
             Process.Start("notepad.exe");
@@ -47,6 +60,10 @@ class Sandboxing
             // COM server creation
             return Activator.CreateInstance(clsid);
         })!;
+
+        token.Dispose();
+
+        return obj;
     }
 
     // based on https://github.com/dotnet/wpf-test/blob/main/src/Test/Common/Code/Microsoft/Test/Diagnostics/ProcessHelper.cs
@@ -79,4 +96,35 @@ class Sandboxing
 
     [DllImport("Advapi32.dll")]
     private static extern int GetLengthSid(IntPtr pSid);
+
+    internal enum SECURITY_IMPERSONATION_LEVEL : uint
+    {
+        SecurityAnonymous = 0x0u,
+        SecurityIdentification = 0x1u,
+        SecurityImpersonation = 0x2u,
+        SecurityDelegation = 0x3u,
+    }
+
+    internal enum TokenType : int
+    {
+        TokenPrimary = 1,
+        TokenImpersonation
+    }
+
+    [DllImport("Advapi32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool DuplicateTokenEx(
+                                  SafeAccessTokenHandle hExistingToken,
+                                  TokenAccessLevels dwDesiredAccess,
+                                  IntPtr lpTokenAttributes,   // LPSECURITY_ATTRIBUTES,
+                                  SECURITY_IMPERSONATION_LEVEL ImpersonationLevel,
+                                  TokenType TokenType,
+                                  ref SafeAccessTokenHandle phNewToken);
+
+    [DllImport("advapi32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool OpenProcessToken(IntPtr ProcessHandle, TokenAccessLevels DesiredAccess, out SafeAccessTokenHandle TokenHandle);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    internal static extern IntPtr GetCurrentProcess();
 }
