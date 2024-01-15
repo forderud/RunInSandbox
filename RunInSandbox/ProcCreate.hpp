forderud/RunInSandbox

commit 84e6736a467d10591fd4d1856c43d9c1b4458735

Switch from using "primary" to "impersonation" token.
The changes have already been tested in the following configurations:
* From elevated (admin) host: "li", "my" and "ac" for TestControl.TestControl
* From regular (non-admin) host: "li", "my" and "ac" for TestControl.TestControl

RunInSandbox/Sandboxing.hpp
@@ -466,8 +466,8 @@ struct ImpersonateThread {
         {
             // current user
             HandleWrap cur_token;
-            WIN32_CHECK(OpenProcessToken(proc, TOKEN_DUPLICATE | TOKEN_ADJUST_DEFAULT | TOKEN_QUERY | TOKEN_ASSIGN_PRIMARY, cur_token.GetAddressOf()));
-            WIN32_CHECK(DuplicateTokenEx(cur_token.Get(), 0, NULL, SecurityImpersonation, TokenPrimary, m_token.GetAddressOf()));
+            WIN32_CHECK(OpenProcessToken(proc, TOKEN_DUPLICATE | TOKEN_ADJUST_DEFAULT | TOKEN_QUERY | TOKEN_IMPERSONATE, cur_token.GetAddressOf()));
+            WIN32_CHECK(DuplicateTokenEx(cur_token.Get(), 0, NULL, SecurityImpersonation, TokenImpersonation, m_token.GetAddressOf()));
         }
 
         if (integrity != IntegrityLevel::Default)
@@ -478,8 +478,8 @@ struct ImpersonateThread {
 
     ImpersonateThread(HandleWrap & handle) {
         HandleWrap cur_token;
-        WIN32_CHECK(OpenProcessToken(handle.Get(), TOKEN_DUPLICATE | TOKEN_ADJUST_DEFAULT | TOKEN_QUERY | TOKEN_ASSIGN_PRIMARY, cur_token.GetAddressOf()));
-        WIN32_CHECK(DuplicateTokenEx(cur_token.Get(), 0, NULL, SecurityImpersonation, TokenPrimary, m_token.GetAddressOf()));
+        WIN32_CHECK(OpenProcessToken(handle.Get(), TOKEN_DUPLICATE | TOKEN_ADJUST_DEFAULT | TOKEN_QUERY | TOKEN_IMPERSONATE, cur_token.GetAddressOf()));
+        WIN32_CHECK(DuplicateTokenEx(cur_token.Get(), 0, NULL, SecurityImpersonation, TokenImpersonation, m_token.GetAddressOf()));
 
         WIN32_CHECK(ImpersonateLoggedOnUser(m_token.Get())); // change current thread integrity
     }

RunInSandboxNet/Sandboxing.cs
@@ -15,8 +15,8 @@ class Sandboxing
      *  WARNING: Buggy code that fail when called multiple times. */
     public static object CoCreate(string level, Type clsid)
     {
-        // matches OpenProcessToken(GetCurrentProcess(), TOKEN_DUPLICATE | TOKEN_ADJUST_DEFAULT | TOKEN_QUERY | TOKEN_ASSIGN_PRIMARY)
-        using WindowsIdentity curToken = WindowsIdentity.GetCurrent(TokenAccessLevels.Duplicate | TokenAccessLevels.AdjustDefault | TokenAccessLevels.Query | TokenAccessLevels.AssignPrimary);
+        // matches OpenProcessToken(GetCurrentProcess(), TOKEN_DUPLICATE | TOKEN_ADJUST_DEFAULT | TOKEN_QUERY | TOKEN_IMPERSONATE)
+        using WindowsIdentity curToken = WindowsIdentity.GetCurrent(TokenAccessLevels.Duplicate | TokenAccessLevels.AdjustDefault | TokenAccessLevels.Query | TokenAccessLevels.Impersonate);
         using var token = (WindowsIdentity)curToken.Clone();
 
         IntPtr sidPtr = IntPtr.Zero;

commit 635cef17c681aed411eca8afefbca805441132fd

Convert impersonation token to primary token before calling CreateProcessAsUserW
This also introduces the need for granting the imp. token TOKEN_ASSIGN_PRIMARY permissions.

Fixes regression introduced by 84e6736a467d10591fd4d1856c43d9c1b4458735

RunInSandbox/ProcCreate.hpp
@@ -130,7 +130,12 @@ static ProcessHandles CreateSuspendedProcess(StartupInfoWrap & si, const wchar_t
             // impersonate desired integrity level
             ImpersonateThread low_int(mode, GetCurrentProcess());
             std::wcout << L"Impersonation succeeded.\n";
-            WIN32_CHECK(CreateProcessAsUserW(low_int.m_token.Get(), exe_path, cmdline.data(), /*proc.attr*/nullptr, /*thread attr*/nullptr, INHERIT_HANDLES, creation_flags, /*env*/nullptr, /*cur-dir*/nullptr, (STARTUPINFO*)&si, &pi));
+
+            // convert to primary token as needed by CreateProcessAsUserW
+            HandleWrap prim_token;
+            WIN32_CHECK(DuplicateTokenEx(low_int.m_token.Get(), 0, NULL, SecurityImpersonation, TokenPrimary, prim_token.GetAddressOf()));
+
+            WIN32_CHECK(CreateProcessAsUserW(prim_token.Get(), exe_path, cmdline.data(), /*proc.attr*/nullptr, /*thread attr*/nullptr, INHERIT_HANDLES, creation_flags, /*env*/nullptr, /*cur-dir*/nullptr, (STARTUPINFO*)&si, &pi));
         } else {
             // use STARTUPINFO to determine integrity level
             WIN32_CHECK(CreateProcessW(exe_path, cmdline.data(), /*proc.attr*/nullptr, /*thread attr*/nullptr, INHERIT_HANDLES, creation_flags, /*env*/nullptr, /*cur-dir*/nullptr, (STARTUPINFO*)&si, &pi));

RunInSandbox/Sandboxing.hpp
@@ -466,7 +466,7 @@ struct ImpersonateThread {
         {
             // current user
             HandleWrap cur_token;
-            WIN32_CHECK(OpenProcessToken(proc, TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_ADJUST_DEFAULT, cur_token.GetAddressOf()));
+            WIN32_CHECK(OpenProcessToken(proc, TOKEN_ASSIGN_PRIMARY | TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_ADJUST_DEFAULT, cur_token.GetAddressOf()));
             WIN32_CHECK(DuplicateTokenEx(cur_token.Get(), 0, NULL, SecurityImpersonation, TokenImpersonation, m_token.GetAddressOf()));
         }
 
@@ -478,7 +478,7 @@ struct ImpersonateThread {
 
     ImpersonateThread(HandleWrap & handle) {
         HandleWrap cur_token;
-        WIN32_CHECK(OpenProcessToken(handle.Get(), TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_ADJUST_DEFAULT, cur_token.GetAddressOf()));
+        WIN32_CHECK(OpenProcessToken(handle.Get(), TOKEN_ASSIGN_PRIMARY | TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_ADJUST_DEFAULT, cur_token.GetAddressOf()));
         WIN32_CHECK(DuplicateTokenEx(cur_token.Get(), 0, NULL, SecurityImpersonation, TokenImpersonation, m_token.GetAddressOf()));
 
         WIN32_CHECK(ImpersonateLoggedOnUser(m_token.Get())); // change current thread integrity