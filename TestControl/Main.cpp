47ac7b5d38fee075b7b32f9bfc049edc93449707
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-05-22 21:26:07 +0200

    Move CoInitializeSecurity call to InitializeSecurity method.

--- TestControl/Main.cpp
+++ TestControl/Main.cpp
@@ -21,6 +21,16 @@ public:
 
     DECLARE_LIBID(LIBID_TestControl)
     DECLARE_REGISTRY_APPID_RESOURCEID(IDR_AppID, "{264FBADA-8FEF-44B7-801E-B728A1749B5A}")
+
+    HRESULT InitializeSecurity() {
+        // Disable COM security to allow any client to connect.
+        // WARNING: Enables non-admin clients to connect to a server running with admin privileges.
+        HRESULT hr = CoInitializeSecurity(nullptr, -1/*auto*/, nullptr, NULL/*reserved*/,
+            RPC_C_AUTHN_LEVEL_DEFAULT, ///< 
+            RPC_C_IMP_LEVEL_IDENTIFY,  ///< allow server to identify but not impersonate client
+            nullptr, EOAC_NONE/*capabilities*/, NULL/*reserved*/);
+        return hr;
+    }
 };
 #endif
 
@@ -95,13 +105,7 @@ STDAPI DllInstall(BOOL bInstall, _In_opt_  LPCWSTR pszCmdLine) {
 int wWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, wchar_t* /*lpCmdLine*/, int nShowCmd/*=SW_SHOWDEFAULT*/) {
     // initialize COM early for programmatic COM security
     _AtlModule.InitializeCom();
-
-    // Disable COM security to allow any client to connect.
-    // WARNING: Enables non-admin clients to connect to a server running with admin privileges.
-    HRESULT hr = CoInitializeSecurity(nullptr, -1/*auto*/, nullptr, NULL/*reserved*/,
-        RPC_C_AUTHN_LEVEL_DEFAULT, ///< 
-        RPC_C_IMP_LEVEL_IDENTIFY,  ///< allow server to identify but not impersonate client
-        nullptr, EOAC_NONE/*capabilities*/, NULL/*reserved*/);
+    HRESULT hr = _AtlModule.InitializeSecurity();
     if (FAILED(hr))
         abort();
 
