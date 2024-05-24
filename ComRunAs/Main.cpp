7ef4638835b5265f37b089a66ceafb12cc362b7a
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-05-24 13:34:50 +0200

    Initial untested impl.

--- ComRunAs/Main.cpp
+++ ComRunAs/Main.cpp
@@ -1,6 +1,20 @@
 #include "RunAs.hpp"
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!\n";
+int wmain(int argc, wchar_t* argv[]) {
+    if (argc < 3) {
+        wprintf(L"USAGE: ComRunAs.exe <AppID> <UserName> <Password>\n");
+        return 1;
+    }
+
+    WCHAR* appid = argv[1];
+    WCHAR* username = argv[2];
+    WCHAR* password = nullptr;
+    if (argc >= 4)
+        password = argv[3]; // optional
+
+    wprintf(L"Configuring COM server with AppID %s to run with user %s.\n", appid, username);
+
+    auto res = SetRunAsAccount(appid, username, password);
+    return res;
 }
