6fe043feccc6aeb67eb5e9cb54d2cb1d2e7926ed
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-05-28 09:44:36 +0200

    Extend instructions for use.

--- ComRunAs/Main.cpp
+++ ComRunAs/Main.cpp
@@ -11,9 +11,9 @@ int wmain(int argc, wchar_t* argv[]) {
     if (argc < 2) {
         wprintf(L"USAGE: ComRunAs.exe <AppID> [UserName] [Password]\n");
         wprintf(L"Examples:\n");
-        wprintf(L"  ComRunAs.exe {99999999-9999-9999-9999-00AA00BBF7C7}\n");
-        wprintf(L"  ComRunAs.exe {99999999-9999-9999-9999-00AA00BBF7C7} \"nt authority\\localservice\"\n");
-        wprintf(L"  ComRunAs.exe {99999999-9999-9999-9999-00AA00BBF7C7} MyAccount MyPassword\n");
+        wprintf(L"  Launching user (default): ComRunAs.exe {99999999-9999-9999-9999-00AA00BBF7C7}\n");
+        wprintf(L"  LocalService account    : ComRunAs.exe {99999999-9999-9999-9999-00AA00BBF7C7} \"nt authority\\localservice\"\n");
+        wprintf(L"  Other account           : ComRunAs.exe {99999999-9999-9999-9999-00AA00BBF7C7} MyAccount MyPassword\n");
         return 1;
     }
 
