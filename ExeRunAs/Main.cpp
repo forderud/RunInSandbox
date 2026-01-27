923ee2bf05cba00a18e5a517e1885d7633138f0e
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-01-27 15:41:03 +0100

    .

--- ExeRunAs/Main.cpp
+++ ExeRunAs/Main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <Shlobj.h> // for IsUserAnAdmin
+
 
 int wmain (int argc, wchar_t* argv[]) {
+    if (!IsUserAnAdmin()) {
+        wprintf(L"ERROR: Admin privileges required.\n");
+        return -1;
+    }
+
+    if (argc < 2) {
+        wprintf(L"USAGE: ExeRunAs.exe [UserName] <command>\n");
+        wprintf(L"Examples:\n");
+        wprintf(L"  LocalService account: ExeRunAs.exe LocalService cmd.exe\n");
+        return 1;
+    }
+
     wprintf(L"Not yet implemented.\n");
 }
